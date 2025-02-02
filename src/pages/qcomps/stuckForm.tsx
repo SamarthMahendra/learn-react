4d16eefe5a0230cd1232655fd5c39cfdeff24ab7
M Samarth Mahendra <39624187+SamarthMahendra@users.noreply.github.com>
2025-02-01 19:38:01 -0500

    fixed bug in stuckform , used react state instead of local variable

--- src/pages/qcomps/stuckForm.tsx
+++ src/pages/qcomps/stuckForm.tsx
@@ -1,34 +1,40 @@
+import React, { useState } from 'react';
+
 export default function Form() {
-  let firstName = '';
-  let lastName = '';
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
 
   function handleFirstNameChange(e: { target: { value: string; }; }) {
-    firstName = e.target.value;
+      setFirstName(e.target.value);
   }
 
   function handleLastNameChange(e: { target: { value: string; }; }) {
-    lastName = e.target.value;
+      setLastName(e.target.value);
   }
 
   function handleReset() {
-    firstName = '';
-    lastName = '';
+      setFirstName('');
+      setLastName('');
   }
 
-  return (
-    <form onSubmit={e => e.preventDefault()}>
-      <input
-        placeholder="First name"
-        value={firstName}
-        onChange={handleFirstNameChange}
-      />
-      <input
-        placeholder="Last name"
-        value={lastName}
-        onChange={handleLastNameChange}
-      />
-      <h1>Hi, {firstName} {lastName}</h1>
-      <button onClick={handleReset}>Reset</button>
-    </form>
-  );
+    return (
+        <form onSubmit={e => e.preventDefault()}>
+            <input
+                placeholder="First name"
+                value={firstName}
+                onChange={handleFirstNameChange}
+            />
+            <input
+                placeholder="Last name"
+                value={lastName}
+                onChange={handleLastNameChange}
+            />
+            <h1>
+                Hi, {firstName} {lastName}
+            </h1>
+            <button type="button" onClick={handleReset}>
+                Reset
+            </button>
+        </form>
+    );
 }
